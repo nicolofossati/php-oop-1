5083c814d7552432635b63efb7980a37e4690cce
nicolo fossati <nicofossa01@gmail.com>
2023-05-04 16:09:18 +0200

    inserimento metodi get e set  classe Movie

--- index.php
+++ index.php
@@ -17,9 +17,67 @@ class Movie
         $this->runtime = $runtime;
         $this->procution_house = $procution_house;
     }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getOriginal_language()
+    {
+        return $this->original_language;
+    }
+
+    public function getRelease_date()
+    {
+        return $this->release_date;
+    }
+    public function getGenres()
+    {
+        return $this->genres;
+    }
+    public function getRuntime()
+    {
+        return $this->runtime;
+    }
+    public function getProcution_house()
+    {
+        return $this->procution_house;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setOriginal_language($original_language)
+    {
+        $this->original_language = $original_language;
+    }
+
+    public function setRelease_date($release_date)
+    {
+        $this->release_date = $release_date;
+    }
+    public function setGenres($genres)
+    {
+        $this->genres = $genres;
+    }
+    public function setRuntime($runtime)
+    {
+        $this->runtime = $runtime;
+    }
+    public function setProcution_house($procution_house)
+    {
+        $this->procution_house = $procution_house;
+    }
 }
 
 $ciao = new Movie('ciao', 'english', 2015, 'horror', 125, 'WB');
-
+echo $ciao->getTitle();
+?></br>
+<?php
+$ciao->setTitle('ciaoo');
+echo $ciao->getTitle();
 
 ?>
