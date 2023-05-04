6d0cdae936513254b91fc0c29b775ca13336e414
nicolo fossati <nicofossa01@gmail.com>
2023-05-04 15:36:13 +0200

    inserimento attributi

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 class Movie
 {
-
+    public $title;
+    public $original_language;
+    public $release_date;
+    public $genres;
+    public $runtime;
+    public $procution_house;
 }
 
 ?>
