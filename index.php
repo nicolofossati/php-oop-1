0254b563d01ee1e59811f3e845756f26f4c9f4ac
nicolo fossati <nicofossa01@gmail.com>
2023-05-04 15:51:11 +0200

    inserimento costruttore

--- index.php
+++ index.php
@@ -7,6 +7,19 @@ class Movie
     public $genres;
     public $runtime;
     public $procution_house;
+
+    public function __construct($title, $original_language, $release_date, $genres, $runtime, $procution_house)
+    {
+        $this->title = $title;
+        $this->original_language = $original_language;
+        $this->release_date = $release_date;
+        $this->genres = $genres;
+        $this->runtime = $runtime;
+        $this->procution_house = $procution_house;
+    }
 }
+
+$ciao = new Movie('ciao', 'english', 2015, 'horror', 125, 'WB');
+
 
 ?>
